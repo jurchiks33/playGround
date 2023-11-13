ceecdbb2882ca7e3d39c5331be47de7cafa5640a
jurchiks33 <jurisslisans@gmail.com>
2023-11-13 19:09:55 +0100

    basics

---
+++ playGround.py
@@ -0,0 +1,16 @@
+                    #Variables
+# message = "Hello Python World"
+# print(message)
+
+# message = "Hello Python World 2"
+# print(message)
+
+# message = "I am an error message"
+# print(mesage)
+                    #Capital Letters
+# name = "ada lovelace"
+# print(name.title())
+
+name = "ada Lovelace"
+print(name.upper())
+print(name.lower())
