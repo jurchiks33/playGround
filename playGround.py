cb4e6e93ae3e81d16189bf0f10e086326772c10b
jurchiks33 <jurisslisans@gmail.com>
2023-11-14 20:08:51 +0100

    lists

--- playGround.py
+++ playGround.py
@@ -98,3 +98,10 @@
 # print(motorcycles)
 # motorcycles.remove('ducati')
 # print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
