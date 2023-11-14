191cfdbe6c28e30f39332321d37b9862009e3c06
jurchiks33 <jurisslisans@gmail.com>
2023-11-14 20:02:43 +0100

    lists

--- playGround.py
+++ playGround.py
@@ -84,8 +84,12 @@
 # del motorcycles[0]
 # print(motorcycles)
 
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+# popped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
 motorcycles = ['honda', 'yamaha', 'suzuki']
-print(motorcycles)
-popped_motorcycle = motorcycles.pop()
-print(motorcycles)
-print(popped_motorcycle)
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
