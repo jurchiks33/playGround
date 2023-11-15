daef30c80101e03de955497b7b0e74dc947d28f7
jurchiks33 <jurisslisans@gmail.com>
2023-11-15 17:34:53 +0100

    for loop in a lists

--- playGround.py
+++ playGround.py
@@ -146,3 +146,7 @@
 # for magician in magicians:
 #     print(f"{magician.title()}, that was a great trick")
 
+magicians = ['alice', 'david', 'caroline']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick")
+    print(f"I cant wait to see your next trick, {magician.title()} \n")
