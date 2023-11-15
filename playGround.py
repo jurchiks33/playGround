3b1933fbc5946c14566298561d58548369309679
jurchiks33 <jurisslisans@gmail.com>
2023-11-15 17:58:28 +0100

    erange in lists

--- playGround.py
+++ playGround.py
@@ -163,3 +163,14 @@
 
 # even_numbers = list(range(2, 20, 2))
 # print(even_numbers)
+
+squares = []
+for value in range(1, 20):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+squares = []
+for value in range(1, 20):
+    squares.append(value**2)
+print(squares)
