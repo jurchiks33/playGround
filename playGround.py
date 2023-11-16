c9f6d1b850ded74c29d884ad4d93103654938038
jurchiks33 <jurisslisans@gmail.com>
2023-11-16 16:39:10 +0100

    lists

--- playGround.py
+++ playGround.py
@@ -164,13 +164,21 @@
 # even_numbers = list(range(2, 20, 2))
 # print(even_numbers)
 
-squares = []
-for value in range(1, 20):
-    square = value ** 2
-    squares.append(square)
-print(squares)
+# squares = []
+# for value in range(1, 20):
+#     square = value ** 2
+#     squares.append(square)
+# print(squares)
 
-squares = []
-for value in range(1, 20):
-    squares.append(value**2)
+# squares = []
+# for value in range(1, 20):
+#     squares.append(value**2)
+# print(squares)
+
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# min(digits)
+# max(digits)
+# sum(digits)
+
+squares = [value ** 2 for value in range (1, 11)]
 print(squares)
