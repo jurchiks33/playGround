ad98eb2846a841f14d447dd3f10e37e749327741
jurchiks33 <jurisslisans@gmail.com>
2023-11-17 16:44:37 +0100

    if statements

--- playGround.py
+++ playGround.py
@@ -247,3 +247,10 @@
 # for dimension in dimensions:
 #     print(dimension)
 
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
