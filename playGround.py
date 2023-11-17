9144674895489374bdf92ac51519a0e03e6e8817
jurchiks33 <jurisslisans@gmail.com>
2023-11-17 18:26:07 +0100

    if elif else statements

--- playGround.py
+++ playGround.py
@@ -329,3 +329,25 @@
 #     print("Your admission cost is $25.")
 # else:
 #     print("Your admission cost is $40.")
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# else:
+#     price = 40
+
+# print(f"Your admission cost is ${price}.")
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# else:
+#     price = 20
+# print(f"Your admission price is ${price}.")
+
