ee5430a220d8190cd926d97ed8d2234355b110dc
jurchiks33 <jurisslisans@gmail.com>
2023-11-17 17:17:54 +0100

    if-else statements

--- playGround.py
+++ playGround.py
@@ -314,3 +314,10 @@
 #     print("you are old enought to vote!")
 #     print("Have you registered to vote yet?")
 
+# age = 17
+# if age >= 18:
+#     print("You are old enought to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
