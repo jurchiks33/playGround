c89eb4bf1e78fc4f362ed810691642052a7b315c
jurchiks33 <jurisslisans@gmail.com>
2023-11-17 17:05:41 +0100

    if statements

--- playGround.py
+++ playGround.py
@@ -297,3 +297,8 @@
 # 'pepperoni' in requested_toppings
 # False
 
+# banned_users = ['andrew', 'carolina', 'davide']
+# user = 'marie'
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish")
+
