c2e4e65f8aff3647576cef8db94eff5f970fc662
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 13:45:55 +0100

    dictionaries

--- playGround.py
+++ playGround.py
@@ -440,10 +440,14 @@
 
 # alien_0 = {'color': 'green', 'points': 5}
 # print(alien_0)
-
 # alien_0['x_position'] = 0
 # alien_0['y_position'] = 25
+# print(alien_0)
 
+
+# alien_0 = {}
+# alien_0['color'] = 'green'
+# alien_0['points'] = 5
 # print(alien_0)
 
 
