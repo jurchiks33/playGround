c286a68619716ad6b9adb7e9e5d8aeebbb641b70
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 13:48:42 +0100

    dictionaries

--- playGround.py
+++ playGround.py
@@ -451,3 +451,9 @@
 # print(alien_0)
 
 
+# alien_0 = {'color': 'green'}
+# print(f"The alien is {alien_0['color']}.")
+# alien_0['color'] = 'yellow'
+# print(f"the alien is now {alien_0['color']}.")
+
+
