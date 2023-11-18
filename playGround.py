df6e225501fa8ad5f374a9a9c8004dde3102df73
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 13:38:47 +0100

    dictionaries

--- playGround.py
+++ playGround.py
@@ -429,3 +429,11 @@
 # print(alien_0['color'])
 # print(alien_0['points'])
 
+
+#in PYTHON dictionary is wrapped in {}
+
+
+# alien_0 = {'color': 'green', 'points': 5}
+# new_points = alien_0['points']
+# print(f"You just earned {new_points} points!")
+
