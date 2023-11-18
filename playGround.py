4b9da0868cf3da9bc463c36cdeb33ba8f506ffa8
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 11:22:43 +0100

    if statements

--- playGround.py
+++ playGround.py
@@ -385,3 +385,8 @@
 # if 'orange' in alien_color:
 #     print("congratulations you got a rare alien and 20 points")
 
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    print(f"adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
