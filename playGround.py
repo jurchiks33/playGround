12a9d0825b08abf7bbacc7f8d16f7edd55730364
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 13:28:28 +0100

    multiple lists

--- playGround.py
+++ playGround.py
@@ -407,3 +407,15 @@
 # else:
 #     print("Are you sure you want a plain pizza?")
 
+
+# available_toppings = ['mushrooms', 'olives', 'green peppers',
+#                       'pepperoni', 'pineapple', 'extra cheese']
+
+# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f"adding {requested_topping}.")
+#     else:
+#         print(f"Sorry, we dont have {requested_topping}.")
+# print("\nFinishing making your pizza!")
