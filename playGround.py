d429d1bffce896d837761eee13d36e5b028a5823
jurchiks33 <jurisslisans@gmail.com>
2023-11-18 12:11:53 +0100

    if statements

--- playGround.py
+++ playGround.py
@@ -398,3 +398,12 @@
 #     else:
 #         print(f"adding {requested_topping}.")
 # print("\nFinished making your pizza.")
+
+# requested_toppings = []
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f"adding {requested_topping}.")
+#         print("\nFinishing making your pizza!")
+# else:
+#     print("Are you sure you want a plain pizza?")
+
