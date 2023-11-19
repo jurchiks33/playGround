b45f6daeb8f160135f73b56aae8e5596343f620e
jurchiks33 <jurisslisans@gmail.com>
2023-11-19 20:18:15 +0100

    dictionaries

--- playGround.py
+++ playGround.py
@@ -492,3 +492,13 @@
 # print(point_value)
 
 
+# user_0 = {
+#     'username': 'efermi',
+#     'first': 'enrico',
+#     'last': 'fermi',
+# }
+# for key, value in user_0.items():
+#     print(f"\nKey: {key}")
+#     print(f"Value: {value}")
+
+
