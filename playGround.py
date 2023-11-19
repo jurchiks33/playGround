62e0892e99d627b0dc593f8924051a2a2400b9cf
jurchiks33 <jurisslisans@gmail.com>
2023-11-19 20:50:39 +0100

    list of dictionaries

--- playGround.py
+++ playGround.py
@@ -533,3 +533,11 @@
 #     print(language.title())
 
 
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_1 = {'color': 'yellow', 'points': 10}
+# alien_2 = {'color': 'red', 'points': '15'}
+# aliens = [alien_0, alien_1, alien_2]
+# for alien in aliens:
+#     print(alien)
+
+
