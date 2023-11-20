96ca0fc9bbe20f7e8a8cb9106d1b14d306fb2ad6
jurchiks33 <jurisslisans@gmail.com>
2023-11-20 21:28:52 +0100

    while loop

--- playGround.py
+++ playGround.py
@@ -672,3 +672,11 @@
 #     current_number += 1
 
 
+# prompt = "\nTell me something, and i will repeat it back to you:"
+# prompt += "\Enter 'quit to end program."
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+
