b25ca515e9a77fb3641539e16d5e87ac097e0632
jurchiks33 <jurisslisans@gmail.com>
2023-11-20 19:43:35 +0100

    list in dictionaries

--- playGround.py
+++ playGround.py
@@ -601,3 +601,15 @@
 #     print(f"\t{topping}")
 
 
+# favorite_languages = {
+#     'jen': ['python', 'rust'],
+#     'sarah': ['c'],
+#     'edward': ['rust', 'go'],
+#     'phil': ['python', 'haskall'],
+# }
+# for name, languages in favorite_languages.items():
+#     print(f"\n{name.title()}'s favorite languages are:")
+#     for language in languages:
+#         print(f"\t{language.title()}")
+
+
