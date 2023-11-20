49afe1995fe5e877fcd2034b71ca52a304691fbb
jurchiks33 <jurisslisans@gmail.com>
2023-11-20 19:53:36 +0100

    dictionary in dictionaries

--- playGround.py
+++ playGround.py
@@ -613,15 +613,23 @@
 #         print(f"\t{language.title()}")
 
 
-users = {
-    'aeinstein': {
-        'first': 'albert',
-        'last': 'einstein',
-        'location': 'princeton',
-    },
-    'mcurie': {
-        'first': 'marie',
-        'last': 'curie',
-        'location': 'paris',
-    }
-}
+# users = {
+#     'aeinstein': {
+#         'first': 'albert',
+#         'last': 'einstein',
+#         'location': 'princeton',
+#     },
+#     'mcurie': {
+#         'first': 'marie',
+#         'last': 'curie',
+#         'location': 'paris',
+#     }
+# }
+# for username, user_info in users.items():
+#     print(f"\nUsername: {username}")
+#     full_name = f"{user_info['first']} {user_info['last']}"
+#     location = user_info['location']
+# print(f"\tFull name: {full_name.title()}")
+# print(f"\tLocation: {location.title()}")
+
+
