c88c1664ee2f4ec0a7438efa674019a651fd9b5c
jurchiks33 <jurisslisans@gmail.com>
2023-11-20 16:48:33 +0100

    list of dictionaries

--- playGround.py
+++ playGround.py
@@ -591,3 +591,13 @@
 # print("....")
 
 
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extra cheese'],
+# }
+# print(f"you ordered a {pizza['crust']}-crust pizza "
+#        "with the following toppings:")
+# for topping in pizza['toppings']:
+#     print(f"\t{topping}")
+
+
