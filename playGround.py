d73362f9d680e43c8bac99cff9cbbaed150bc5fd
jurchiks33 <jurisslisans@gmail.com>
2023-11-21 11:49:16 +0100

    continue in a Loop and avoid infinite Loop

--- playGround.py
+++ playGround.py
@@ -701,3 +701,16 @@
 #         print(f"I'd love to go to {city.title()}!")
 
 
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 == 0:
+#         continue
+#     print(current_number)
+
+
+# x = 1
+# while x <= 5:
+#     print(x)
+
+
