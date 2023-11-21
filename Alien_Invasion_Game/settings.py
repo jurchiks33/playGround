479b417a4dea54b60b9b3f0663e53efc2327c7fb
jurchiks33 <jurisslisans@gmail.com>
2023-11-21 13:22:44 +0100

    setting

---
+++ Alien_Invasion_Game/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize game's settings."""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
