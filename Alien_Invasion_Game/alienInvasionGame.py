51eb34579e4eb6a47220d57d1588e5a2a853e447
jurchiks33 <jurisslisans@gmail.com>
2023-11-21 14:44:53 +0100

    Refactoring

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -16,20 +17,27 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #Watch for the keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+    
+    def _check_events(self):
+            """Respond to keypress and mouse events."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
-            #Redraw the screen during each pass throught the loop.
-            self.screen.fill(self.settings.bg_color)
-            
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)
+    
+    def _update_screen(self):
+         """Update images on the screen, and flip to the new screen."""
+         self.screen.fill(self.settings.bg_color)
+         self.ship.blitme()
+
+         pygame.display.flip()
 
 if __name__ == '__main__':
     #Make game instance and run the game.
--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -3,6 +3,6 @@ class Settings:
     def __init__(self):
         """Initialize game's settings."""
         #Screen Settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1500
+        self.screen_height = 900
+        self.bg_color = (36, 33, 36)
