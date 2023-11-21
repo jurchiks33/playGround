c37f7519d63b3ca229c06648d5582fc1e16d8ed1
jurchiks33 <jurisslisans@gmail.com>
2023-11-21 13:06:38 +0100

    setting background color and fixing indentation

--- alienInvasionGame.py
+++ alienInvasionGame.py
@@ -4,23 +4,28 @@ import pygame
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
-def __init__(self):
-    """Initialize game, and create gae resources."""
-    pygame.init
+    def __init__(self):
+        """Initialize game, and create gae resources."""
+        pygame.init
+        self.clock = pygame.time.Clock()
 
-    self.screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
 
-def run_game(self):
-    """Start the main loop for the game"""
-    while True:
-        #Watch for the keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
-        #Make the most recently drawn screen visible.
-        pygame.display.flip()
+        #Set the background color.
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            #Watch for the keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            #Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Make game instance and run the game.
