fb7d8a335b6bd12af632b83a6fc767f93628b143
jurchiks33 <jurisslisans@gmail.com>
2023-11-21 12:52:17 +0100

    setting up main loop and game instance

--- alienInvasionGame.py
+++ alienInvasionGame.py
@@ -7,5 +7,22 @@ class AlienInvasion:
 def __init__(self):
     """Initialize game, and create gae resources."""
     pygame.init
+
     self.screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+
+def run_game(self):
+    """Start the main loop for the game"""
+    while True:
+        #Watch for the keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    #Make game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
