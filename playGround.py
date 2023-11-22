3a3964cadff2e931f368271f03593c07899c3443
jurchiks33 <jurisslisans@gmail.com>
2023-11-22 14:27:13 +0100

    return a simple value

--- playGround.py
+++ playGround.py
@@ -811,3 +811,11 @@
 # #Each of these methods would work with example above
 
 
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+
+
