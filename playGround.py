01e27c9fd8f5931997cc81c8b2c282b741f8bf7a
jurchiks33 <jurisslisans@gmail.com>
2023-11-22 13:47:49 +0100

    multiple function call

--- playGround.py
+++ playGround.py
@@ -778,3 +778,12 @@
 #     print(f"My {animal_type}'s name is {pet_name.title()}.")
 # describe_pet('hamster', 'harry')
 
+
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+# describe_pet('hamster', 'harry')
+# describe_pet('dog', 'willie')
+
+
