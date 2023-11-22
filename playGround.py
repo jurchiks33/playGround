99496cd76cecbbb81b7fc3c60b6570764180582d
jurchiks33 <jurisslisans@gmail.com>
2023-11-22 13:55:32 +0100

    keyword arguments

--- playGround.py
+++ playGround.py
@@ -787,3 +787,10 @@
 # describe_pet('dog', 'willie')
 
 
+# def describe_pet(animal_type, pet_name):
+#     """Display information about a pet."""
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
+# describe_pet(animal_type='hamester', pet_name='harry')
+
+
