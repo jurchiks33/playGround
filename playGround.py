d30952ba34527778612baccea7274c8533213368
jurchiks33 <jurisslisans@gmail.com>
2023-11-22 10:34:35 +0100

    user input

--- playGround.py
+++ playGround.py
@@ -738,3 +738,22 @@
 # print(pets)
 
 
+# responses = {}
+# #Set flag to indicate that pooling is active
+# pooling_active = True
+# while pooling_active:
+#     #Prompt for the persons name and response
+#     name = input("\nWhat is your name?")
+#     response = input("Which mountain would you like to climb someday?")
+#     #Store response in the dictionary.
+#     responses[name] = response
+#     #Find out if anyone else is going to take the pool.
+#     repeat = input("Would you like to let another person respond? (yes/no)")
+#     if repeat == 'no':
+#         pooling_active = False
+#  #Pooling is complete. Show the results.
+#     print("\n---Poll Results---")
+#     for name, response in responses.items():
+#         print(f"{name} would you like to climb {response}.")
+
+
