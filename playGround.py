7bdcc36d6d6691578dee3e84c1a3eeb24b59f9cb
jurchiks33 <jurisslisans@gmail.com>
2023-11-23 12:11:45 +0100

    Modify a list in a function

--- playGround.py
+++ playGround.py
@@ -904,3 +904,20 @@
 # greet_users(usernames)
 
 
+# #Strat with some designs that need to be printed.
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+
+# #Simulate printing each design, until none are left.
+# #Move eash design to completed_models after printing.
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
+
+# #Display all completed models.
+# print("\nThe following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+
+
