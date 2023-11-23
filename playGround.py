df4b104efdeae0abfbfe40dd9542cf8b28602e92
jurchiks33 <jurisslisans@gmail.com>
2023-11-23 12:41:02 +0100

    Passing an arbitrary number of arguments

--- playGround.py
+++ playGround.py
@@ -942,3 +942,21 @@
 # show_completed_models(completed_models)
 
 
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested."""
+#     print(toppings)
+
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+# def make_pizza(*toppings):
+#     """Summirize the pizza we are about to make."""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
