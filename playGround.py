f8941ad4a103b329c3de97a34005c9584fc91837
jurchiks33 <jurisslisans@gmail.com>
2023-11-23 12:03:02 +0100

    Passing a list

--- playGround.py
+++ playGround.py
@@ -894,3 +894,13 @@
 #     print(f"\nHello, {formatted_name}!")
 
 
+# def greet_users(names):
+#     """Print a simple greeting to each user in the list."""
+#     for name in names:
+#         msg = f"Hello, {name.title()}!"
+#         print(msg)
+
+# usernames = ['Hannah', 'ty', 'margot']
+# greet_users(usernames)
+
+
