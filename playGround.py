e830694c3491c9702d094936774907e1d999dfba
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 11:01:57 +0100

    calling methods

--- playGround.py
+++ playGround.py
@@ -1061,3 +1061,24 @@
 # print(f"My dog is {my_dog.age} years old")
 
 
+# class Dog:
+#     """A simple attempt to model a dog."""
+
+#     def __init__(self, name, age):
+#         """Initialize name and age attributes."""
+#         self.name = name
+#         self.age = age
+    
+#     def sit(self):
+#         """Initialize a dog sitting in response to a command"""
+#         print(f"{self.name} is now sitting.")
+    
+#     def roll_over(self):
+#         """Simulate rolling over in response to a command"""
+#         print(f"{self.name} rolled over!")
+
+# my_dog = Dog('Willie', 6)
+# my_dog.sit()
+# my_dog.roll_over()
+
+
