8cdd5c7621d360dc2ef7be43f5a6b9492f1da1d1
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 09:42:44 +0100

    Importing an entire module

--- playGround.py
+++ playGround.py
@@ -982,3 +982,16 @@
 # print(user_profile)
 
 
+# def make_pizza(size, *toppings):
+#     """Summarize the pizza we are about to make."""
+#     print(f"\nMaking a {size}-inch pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f" - {topping}")
+
+# #This one below should be on a seperate file so we can inport it from
+# import pizza
+
+# pizza.make_pizza(16, 'pepperoni')
+# pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
