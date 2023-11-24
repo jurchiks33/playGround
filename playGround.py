9026d907b9140d9f2d00347d835458495afad08e
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 11:37:29 +0100

    setting default value for an attribute

--- playGround.py
+++ playGround.py
@@ -1122,14 +1122,23 @@
 # print(my_new_car.get_descriptive_name())
 
 
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
     
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
     
+#     def read_odometer(self):
+#         """Print a statement showing the cars mileage."""
+#         print(f"This car has {self.odometer_reading} miles on it")
+
+# my_new_car = Car('audi', 'a4', '2024')
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()  
+
+
