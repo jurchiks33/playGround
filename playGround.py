075896a17c1798b259baa63ef60b8db320b90f54
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 11:33:36 +0100

    setting default value for an attribute

--- playGround.py
+++ playGround.py
@@ -1122,3 +1122,14 @@
 # print(my_new_car.get_descriptive_name())
 
 
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
