7290bec71e108cbeaac2b6714bfa312478841552
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 10:31:09 +0100

    Styling Function

--- playGround.py
+++ playGround.py
@@ -1010,3 +1010,13 @@
 # from pizza import *
 
 
+# def function_name(parameter_0, parameter_1='default value')
+
+# function_name(value_0, parameter_1='value')
+
+# def function_name(
+#         parameter_0, parameter_1, parameter_2,
+#         parameter_3, parameter_4, parameter_5):
+#     function body......
+
+
