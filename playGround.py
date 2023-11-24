47210fddf7fb730ba32082bccb842096f5999590
jurchiks33 <jurisslisans@gmail.com>
2023-11-24 10:53:25 +0100

    creating a class

--- playGround.py
+++ playGround.py
@@ -1020,3 +1020,23 @@
 #     function body......
 
 
+                            #Clases
+
+
+# class Dog:
+#     """A simple attempt to model a dog."""
+
+#     def __init__(self, name, age):
+#         """Initialize name and age attributes."""
+#         self.name = name
+#         self.age = age
+    
+#     def sit(self):
+#         """Initialize a dog sitting in response to a command"""
+#         print(f"{self.name} is now sitting.")
+    
+#     def roll_over(self):
+#         """Simulate rolling over in response to a command"""
+#         print(f"{self.name} rolled over!")
+
+
