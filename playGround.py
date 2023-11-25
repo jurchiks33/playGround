e5a7aeaf77d94cb2372a4bccfdc727aaa0321a9f
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 14:28:39 +0100

    init method for a child class

--- playGround.py
+++ playGround.py
@@ -1250,3 +1250,24 @@
 # my_used_car.read_odometer()
 
 
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self. year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
