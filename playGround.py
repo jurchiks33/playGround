7441115921933af05e094176f276302b5ed83595
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 12:40:12 +0100

    Modify an attribute directly

--- playGround.py
+++ playGround.py
@@ -1142,3 +1142,25 @@
 # my_new_car.read_odometer()  
 
 
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+    
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+    
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
+
+# my_new_car = Car('audi', 'a4', 2024)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
+
+
