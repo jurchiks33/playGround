65a9079e427cc9299730290884cbec07faa81134
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 14:25:44 +0100

    Incrementing an attributes value throught a method

--- playGround.py
+++ playGround.py
@@ -1218,3 +1218,35 @@
 # my_new_car.read_odometer()
 
 
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 0
+    
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+    
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
+    
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value."""
+#         self.odometer_reading = mileage
+
+#     def increment_odometer(self, miles):
+#         """Add the given amount to the odometer reading."""
+#         self.odometer_reading += miles
+
+# my_used_car = Car('subaru', 'outback', 2019)
+# print(my_used_car.get_descriptive_name())
+
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
+
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
+
+
