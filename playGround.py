2fa0130b1f89d8f0f37caea00c2c51e8f0e19dff
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 14:51:35 +0100

    Defining attributes and methods for  child class

--- playGround.py
+++ playGround.py
@@ -1282,3 +1282,30 @@
 # print(my_leaf.get_descriptive_name())
 
 
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self. year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Represents aspect of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
