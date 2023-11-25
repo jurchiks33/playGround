395a8495eba3c14926124c938d51e82424a150bc
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 12:58:51 +0100

    Modify an attribute value throught a method

--- playGround.py
+++ playGround.py
@@ -1164,3 +1164,28 @@
 
 
 
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 0
+    
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+    
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
+    
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value."""
+#         self.odometer_reading = mileage
+
+# my_new_car = Car('audi', 'a4', 2024)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.update_odometer(23)
+# my_new_car.read_odometer()
+
+
