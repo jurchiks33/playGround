293e48b875b79b48438930665368e28530eabc62
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 14:55:55 +0100

    Defining attributes and methods for child class

--- playGround.py
+++ playGround.py
@@ -1282,30 +1282,42 @@
 # print(my_leaf.get_descriptive_name())
 
 
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self. year = year
-        self.odometer_reading = 0
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 0
     
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
     
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it")
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
     
-    def update_odometer(self, mileage):
-        """Set the odometer reading to the given value."""
-        self.odometer_reading = mileage
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value."""
+#         self.odometer_reading = mileage
 
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         """Add the given amount to the odometer reading."""
+#         self.odometer_reading += miles
 
-class ElectricCar(Car):
-    """Represents aspect of a car, specific to electric vehicles."""
-    def __init__(self, make, model, year):
-        """Initialize attributes of the parent class."""
-        super().__init__(make, model, year)
+# class ElectricCar(Car):
+#     """Represents aspect of a car, specific to electric vehicles."""
+#     def __init__(self, make, model, year):
+#         """Initialize attributes of the parent class.
+#         Then initialize attributes specific to an electric car."""
+#         super().__init__(make, model, year)
+#         self.battery_size = 40
+    
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kwh battery.")
+
+# my_leaf = ElectricCar('nissan', 'model', 2024)
+# print(my_leaf.get_descriptive_name())
+# my_leaf.describe_battery()
+
+
