e92658c1659e644373f4ef67e2e0f48795e75566
jurchiks33 <jurisslisans@gmail.com>
2023-11-25 13:02:30 +0100

    modify an attribute value throught method

--- playGround.py
+++ playGround.py
@@ -1189,3 +1189,32 @@
 # my_new_car.read_odometer()
 
 
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 30
+    
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+    
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
+    
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value.
+#         Reject the change if it attempts to roll the odometer back"""
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You cant roll back an odometer.")
+
+# my_new_car = Car('audi', 'a4', 2024)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.update_odometer(25)
+# my_new_car.read_odometer()
+
+
