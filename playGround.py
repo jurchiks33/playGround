10a6ac50b756451378faf98885db72637bdc6cad
jurchiks33 <jurisslisans@gmail.com>
2023-11-26 15:22:51 +0100

    Instances as attributes

--- playGround.py
+++ playGround.py
@@ -1338,51 +1338,113 @@
 #         print("This car doesnt have a gass tank")
 
 
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self. year = year
-        self.odometer_reading = 0
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 0
     
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
     
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it")
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
     
-    def update_odometer(self, mileage):
-        """Set the odometer reading to the given value."""
-        self.odometer_reading = mileage
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value."""
+#         self.odometer_reading = mileage
 
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         """Add the given amount to the odometer reading."""
+#         self.odometer_reading += miles
 
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
+# class Battery:
+#     """A simple attempt to model a battery for an electric car."""
 
-    def __init__(self, battery_size=40):
-        """Initialize the batterys attributes."""
-        self.battery_size = battery_size
+#     def __init__(self, battery_size=40):
+#         """Initialize the batterys attributes."""
+#         self.battery_size = battery_size
     
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kwh battery")
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kwh battery")
 
-class ElectricCar(Car):
-    """Represents aspect of a car, specific to electric vehicles."""
-    def __init__(self, make, model, year):
-        """Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car."""
-        super().__init__(make, model, year)
-        self.battery  = Battery()
+# class ElectricCar(Car):
+#     """Represents aspect of a car, specific to electric vehicles."""
+#     def __init__(self, make, model, year):
+#         """Initialize attributes of the parent class.
+#         Then initialize attributes specific to an electric car."""
+#         super().__init__(make, model, year)
+#         self.battery  = Battery()
     
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kwh battery.")
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kwh battery.")
 
-my_leaf = ElectricCar('nissan', 'model', 2024)
-print(my_leaf.get_descriptive_name())
-my_leaf.battery.describe_battery()
+# my_leaf = ElectricCar('nissan', 'model', 2024)
+# print(my_leaf.get_descriptive_name())
+# my_leaf.battery.describe_battery()
+
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self. year = year
+#         self.odometer_reading = 0
+    
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+    
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it")
+    
+#     def update_odometer(self, mileage):
+#         """Set the odometer reading to the given value."""
+#         self.odometer_reading = mileage
+
+#     def increment_odometer(self, miles):
+#         """Add the given amount to the odometer reading."""
+#         self.odometer_reading += miles
+
+# class Battery:
+#     """A simple attempt to model a battery for an electric car."""
+
+#     def __init__(self, battery_size=65):
+#         """Initialize the batterys attributes."""
+#         self.battery_size = battery_size
+    
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kwh battery")
+    
+#     def get_range(self):
+#         """Print a statement about the range this battery provides."""
+#         if self.battery_size == 40:
+#             range = 150
+#         elif self.battery_size == 65:
+#             range = 225
+
+#             print(f"This car can go about {range} miles on a full charge/")
+
+# class ElectricCar(Car):
+#     """Represents aspect of a car, specific to electric vehicles."""
+#     def __init__(self, make, model, year):
+#         """Initialize attributes of the parent class.
+#         Then initialize attributes specific to an electric car."""
+#         super().__init__(make, model, year)
+#         self.battery  = Battery()
+    
+#     def describe_battery(self):
+#         """Print a statement describing the battery size."""
+#         print(f"This car has a {self.battery_size}-kwh battery.")
+
+# my_leaf = ElectricCar('nissan', 'model', 2024)
+# print(my_leaf.get_descriptive_name())
+# my_leaf.battery.describe_battery()
+# my_leaf.battery.get_range()
+
+
