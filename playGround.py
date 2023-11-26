8e71722e592d300bff1ec770f97cb1fc061fd94a
jurchiks33 <jurisslisans@gmail.com>
2023-11-26 17:13:05 +0100

    Storing multiple classes in a module

--- playGround.py
+++ playGround.py
@@ -1481,3 +1481,25 @@
 # my_new_car.read_odometer()
 
 
+#Storing multiple classes in a module
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self. year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
