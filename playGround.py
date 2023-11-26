612b7e91b2d18b653acfaa6843120dcd0f639b2f
jurchiks33 <jurisslisans@gmail.com>
2023-11-26 16:46:26 +0100

    Importing a single class

--- playGround.py
+++ playGround.py
@@ -1459,3 +1459,16 @@ class Car:
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        print(f"This car has{self.odometer_reading} miles on it")
+    
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You cant roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
