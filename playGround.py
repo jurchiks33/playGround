8d56598d8a18b7ff3a6337301253bd4a5ce2d577
jurchiks33 <jurisslisans@gmail.com>
2023-11-26 16:48:28 +0100

    Importing a single class

--- playGround.py
+++ playGround.py
@@ -1448,27 +1448,36 @@
 # my_leaf.battery.get_range()
 
 
-"""A class that can be used to represent a car"""
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+# """A class that can be used to represent a car"""
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
     
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
     
-    def read_odometer(self):
-        print(f"This car has{self.odometer_reading} miles on it")
+#     def read_odometer(self):
+#         print(f"This car has{self.odometer_reading} miles on it")
     
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You cant roll back an odometer!")
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You cant roll back an odometer!")
 
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
+
+# #Now we import from different py file code above to make it more clean looking.
+# from car import Car
+
+# my_new_car = Car('audi', 'a4', 2024)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+
 
