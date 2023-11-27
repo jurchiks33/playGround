ff3103dfd046bd5bc9e94df00d273a9eae166e4b
jurchiks33 <jurisslisans@gmail.com>
2023-11-27 18:06:26 +0100

    updating ships movement

--- Alien_Invasion_Game/ship.py
+++ Alien_Invasion_Game/ship.py
@@ -13,6 +13,14 @@ class Ship:
 
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #movement flag starting with ship that is not moving.
+        self.move_right = False
+    
+    def update(self):
+        """Update the ships position based on the movement flag."""
+        if self.move_right:
+            self.rect.x += 1
     
     def blitme(self):
         """Draw the ship at its current location."""
