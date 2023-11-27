e4b8f3dfd0cf89dadd37ffccf81072c2952cd929
jurchiks33 <jurisslisans@gmail.com>
2023-11-27 18:27:33 +0100

    updating ship movements

--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -6,3 +6,6 @@ class Settings:
         self.screen_width = 1500
         self.screen_height = 900
         self.bg_color = (36, 33, 36)
+
+        #Ship settings
+        self.ship_speed = 1.5
--- Alien_Invasion_Game/ship.py
+++ Alien_Invasion_Game/ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
@@ -14,13 +15,19 @@ class Ship:
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a float for the ships exact horizontal position
+        self.x = float(self.rect.x)
+
         #movement flag starting with ship that is not moving.
         self.move_right = False
+        self.move_left = False
     
     def update(self):
         """Update the ships position based on the movement flag."""
+        #Update ships x value not the rect
+
         if self.move_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
     
     def blitme(self):
         """Draw the ship at its current location."""
