68beaf743239b5524c00a4a4fad91024e99e2307
jurchiks33 <jurisslisans@gmail.com>
2023-11-27 17:28:45 +0100

    work on a keypress

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -31,6 +31,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                     if event.key == pygame.K_RIGHT:
+                          #Move ship to the right.
+                          self.ship.rect.x += 1
     
     def _update_screen(self):
          """Update images on the screen, and flip to the new screen."""
