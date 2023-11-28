87528fe2a6cea8e6887ed7a5e1a79ed9e288e6b5
jurchiks33 <jurisslisans@gmail.com>
2023-11-28 14:57:52 +0100

    fullscreen

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -13,6 +13,10 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
