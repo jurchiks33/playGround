acdc79c13a44ac02c6c92ced5a43e19ed0df286a
jurchiks33 <jurisslisans@gmail.com>
2023-11-28 15:21:07 +0100

    work on a bullets

--- Alien_Invasion_Game/bullet.py
+++ Alien_Invasion_Game/bullet.py
@@ -10,4 +10,11 @@ class Bullet(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
-        
+
+        #Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #Store bullet position as a float
+        self.y = float(self.rect.y)
