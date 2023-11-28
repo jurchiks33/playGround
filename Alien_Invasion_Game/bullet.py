dddef79252f02658eb1a3acb71f7dc1d3fdeeb3c
jurchiks33 <jurisslisans@gmail.com>
2023-11-28 15:23:38 +0100

    work on a bullet

--- Alien_Invasion_Game/bullet.py
+++ Alien_Invasion_Game/bullet.py
@@ -18,3 +18,14 @@ class Bullet(Sprite):
 
         #Store bullet position as a float
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Move bullet up the screen"""
+        #Update exact position of the bullet.
+        self.y -= self.settings.bullet_speed
+        #Update the rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet on the screen."""
+        pygame.draw.rect(self.screen, self. color, self.rect)
