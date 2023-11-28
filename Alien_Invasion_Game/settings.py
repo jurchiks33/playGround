79ef5e2a29367be0c4d6831fff5a0070607ab16d
jurchiks33 <jurisslisans@gmail.com>
2023-11-28 15:03:26 +0100

    creating a bullet

--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -9,3 +9,9 @@ class Settings:
 
         #Ship settings
         self.ship_speed = 2.5
+
+        #Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
