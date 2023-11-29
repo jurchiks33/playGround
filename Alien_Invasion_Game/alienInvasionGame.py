27e7821fec1c3d4e0137f21181b9f447e68d2aa1
jurchiks33 <jurisslisans@gmail.com>
2023-11-29 14:01:53 +0100

    refactoring

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -91,12 +91,15 @@ class AlienInvasion:
 
         current_x = alien_width
         while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
-        # self.aliens.add(alien)
+            self._create_alien(current_x)
+            current_x += 2* alien_width
+    
+    def _create_alien(self, x_position):
+        """Create an alien and place it in the row."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
          """Update images on the screen, and flip to the new screen."""
