c4e3c5cf71df9bb499fde5b13e3271b0cf2b0606
jurchiks33 <jurisslisans@gmail.com>
2023-12-01 11:56:03 +0100

    mouse cursor dissapears after game active

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -87,6 +87,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            #Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         """Respond to keypress"""
         if event.key == pygame.K_RIGHT:
@@ -211,6 +214,7 @@ class AlienInvasion:
 
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
