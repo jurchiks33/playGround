b86d8c6b66cb40ebd00439ea332c29f6b359a114
jurchiks33 <jurisslisans@gmail.com>
2023-12-01 11:52:36 +0100

    work on a button

--- Alien_Invasion_Game/alienInvasionGame.py
+++ Alien_Invasion_Game/alienInvasionGame.py
@@ -73,7 +73,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
             #Reset the game statistics/
             self.stats.reset_stats()
             self.game_active = True
