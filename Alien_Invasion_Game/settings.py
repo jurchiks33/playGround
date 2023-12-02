6ba3ccfd36f75fcb7daf7957b0ad4442dd8019fa
jurchiks33 <jurisslisans@gmail.com>
2023-12-02 11:00:42 +0100

    working on scoring

--- Alien_Invasion_Game/settings.py
+++ Alien_Invasion_Game/settings.py
@@ -22,6 +22,8 @@ class Settings:
 
         #Game speed up
         self.speedup_scale = 1.1
+        #How  quickly the alien point values increase.
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
     
@@ -42,3 +44,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
