2ca93b240d0e81e28be34a49209998311875a384
jurchiks33 <jurisslisans@gmail.com>
2023-12-02 09:26:00 +0100

    scoring system

--- Alien_Invasion_Game/scoreboard.py
+++ Alien_Invasion_Game/scoreboard.py
@@ -9,3 +9,21 @@ class Scoreboard:
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
+
+        #Font settings for scoring information.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        #Prepare initial score image.
+        self.prep_score()
+
+    def prep_score(self):
+        """Turn the score into rendered image."""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True,
+                            self.text_color, self.settings.bg_color)
+        
+        #display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
