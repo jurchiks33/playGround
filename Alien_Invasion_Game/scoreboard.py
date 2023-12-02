ceffdfe6720a79457fa798a79a5b829242530c27
jurchiks33 <jurisslisans@gmail.com>
2023-12-02 11:12:03 +0100

    work on a high score

--- Alien_Invasion_Game/scoreboard.py
+++ Alien_Invasion_Game/scoreboard.py
@@ -40,3 +40,8 @@ class Scoreboard:
         high_score_str = f"{high_score:,}"
         self.high_score_image = self.font.render(high_score, True,
                         self.text_color, self.settings.bg_color)
+        
+        #Center high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
