13f55c38a9d5b32b1dde2751ee008de7e676a52d
jurchiks33 <jurisslisans@gmail.com>
2023-12-09 20:18:59 +0100

    random walk

--- Plotting_App/random_walk.py
+++ Plotting_App/random_walk.py
@@ -14,14 +14,24 @@ class RabdomWalk:
     def fill_walk(self):
         """Calculate all the points in the walk."""
         #Keep walking while desired lenght is reached.
+        while len(self.x_values) < self.num_points:
 
-        #Decide which direction to go.
-        x_direction = choice([1, -1])
-        x_distance = choice([0, 1, 2, 3, 4])
-        x_step = x_direction * x_distance
+            #Decide which direction to go.
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
 
-        y_direction = choice([1, -1])
-        y_distance = choice([0, 1, 2, 3, 4])
-        y_step = y_direction * y_distance
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
 
-        
+            #Reject moves to go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            #Calculate the new position.
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
