69da400a896a126ea350ad17b0328234f2a0ac2b
jurchiks33 <jurisslisans@gmail.com>
2023-12-09 19:10:08 +0100

    plotting

--- Plotting_App/scatter_squares.py
+++ Plotting_App/scatter_squares.py
@@ -26,18 +26,38 @@ import matplotlib.pyplot as plt
 
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+
+# plt.style.use('seaborn-v0_8')
+# fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, s=100)
+
+# # Setting the size of tick labels
+# ax.tick_params(axis='both', which='major', labelsize=14)
+
+# # Showing the plot
+# plt.show()
+
+# # Saving the plot to a file
+# fig.savefig('scatter_plot.png')
+
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Setting the size of tick labels
-ax.tick_params(axis='both', which='major', labelsize=14)
+ax.axis([0, 1100, 0, 1_100_000])
 
 # Showing the plot
 plt.show()
 
 # Saving the plot to a file
 fig.savefig('scatter_plot.png')
+
+
