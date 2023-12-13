93f2bc59255679dbe0b95a8ab700511cf45fea66
jurchiks33 <jurisslisans@gmail.com>
2023-12-13 13:16:32 +0100

    die class

---
+++ Rolling_Dice_App/die.py
@@ -0,0 +1,12 @@
+from random import randint
+
+class die:
+    """A class representing a single die."""
+
+    def __init__(self, num_sides=6):
+        #For six sided die.
+        self.num_sides = num_sides
+
+    def roll(self):
+        #Returning random value between 1 and number of sides.
+        return randint(1, self.num_sides)
