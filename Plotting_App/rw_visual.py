f930936f274e33aa2dcc1bf2c761bfe331cc8fd7
jurchiks33 <jurisslisans@gmail.com>
2023-12-13 12:16:14 +0100

    looping rw

--- Plotting_App/rw_visual.py
+++ Plotting_App/rw_visual.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RabdomWalk
 
-#Making a random walk.
-rw = RabdomWalk()
-rw.fill_walk()
+#Making loop for a new walks whn programm is active.
+while True:
+    #Making a random walk.
+    rw = RabdomWalk()
+    rw.fill_walk()
 
-#Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
-plt.show()
+    #Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
