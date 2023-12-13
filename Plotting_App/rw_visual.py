90f9816d532378721589aeba3a349fe3865b4cda
jurchiks33 <jurisslisans@gmail.com>
2023-12-13 12:14:01 +0100

    rw plotting

--- Plotting_App/rw_visual.py
+++ Plotting_App/rw_visual.py
@@ -5,3 +5,9 @@ from random_walk import RabdomWalk
 rw = RabdomWalk()
 rw.fill_walk()
 
+#Plot the points in the walk.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+ax.set_aspect('equal')
+plt.show()
