0915e688139625f560144a1fe457ef6eb6b0d47c
jurchiks33 <jurisslisans@gmail.com>
2023-12-13 12:49:40 +0100

    modify plotting

--- Plotting_App/rw_visual.py
+++ Plotting_App/rw_visual.py
@@ -9,7 +9,7 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                edgecolors='none', s=1)
