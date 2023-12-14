b6e3c223e7ef9a88f08fbcc211cdc7bf8307fbc5
jurchiks33 <jurisslisans@gmail.com>
2023-12-14 18:37:27 +0100

    display data

--- Rolling_Dice_App/die_visual.py
+++ Rolling_Dice_App/die_visual.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 from die import Die
 
 #Create D6.
@@ -11,8 +12,12 @@ for roll_num in range(1000):
 #Analyze results.
 frequencies = []
 poss_results = range(1, die.num_sides+1)
-for value in pos_results:
+for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
+
+#Visualize results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
 
 print(results)
