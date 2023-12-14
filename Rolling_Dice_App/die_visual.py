61f3f69faf125670bd31b76547c025b90c40a462
jurchiks33 <jurisslisans@gmail.com>
2023-12-14 19:59:04 +0100

    2 dices

--- Rolling_Dice_App/die_visual.py
+++ Rolling_Dice_App/die_visual.py
@@ -13,13 +13,14 @@ for roll_num in range(1000):
 
 #Analyze results.
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 #Visualize results.
-title = "Results of rolling one D6 1000 times"
+title = "Results of rolling two D6 1000 times"
 labels = {'x', 'Result', 'y', 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
