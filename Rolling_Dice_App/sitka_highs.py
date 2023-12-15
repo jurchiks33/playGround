ac697ba933f3d315789c47e857ff0353907b34aa
jurchiks33 <jurisslisans@gmail.com>
2023-12-15 17:37:45 +0100

    highs and lows plotted

--- Rolling_Dice_App/sitka_highs.py
+++ Rolling_Dice_App/sitka_highs.py
@@ -10,21 +10,24 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-#Extract dates and high temperatures.
-dates, highs = [], []
+#Extract dates and high and low temperatures.
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
-#Plot the high temperatures.
+#Plot the high and low temperatures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 #Format plot.
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
