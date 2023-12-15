a8eda604cfa8877fd3667b81d7a8c3ab2dde9835
jurchiks33 <jurisslisans@gmail.com>
2023-12-15 17:01:27 +0100

    data output

--- Rolling_Dice_App/sitka_highs.py
+++ Rolling_Dice_App/sitka_highs.py
@@ -7,5 +7,10 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+#Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print(highs)
