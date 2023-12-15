8e3cb68a53edbc9ec271ce01b09d31d58db6c9c0
jurchiks33 <jurisslisans@gmail.com>
2023-12-15 16:57:11 +0100

    column info

--- Rolling_Dice_App/sitka_highs.py
+++ Rolling_Dice_App/sitka_highs.py
@@ -6,4 +6,6 @@ lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
