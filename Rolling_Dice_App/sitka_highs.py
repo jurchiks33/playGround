d43b8d4efd3703517c073ac28891b1560b757c87
jurchiks33 <jurisslisans@gmail.com>
2023-12-15 17:20:45 +0100

    fromat plot

--- Rolling_Dice_App/sitka_highs.py
+++ Rolling_Dice_App/sitka_highs.py
@@ -20,6 +20,12 @@ plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(highs, color='red')
 
+#Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
 
+plt.show()
 
 # print(highs)
