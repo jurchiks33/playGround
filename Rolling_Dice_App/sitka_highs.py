10c73b4941005227aa3d7eb41ac54ef8cee3d138
jurchiks33 <jurisslisans@gmail.com>
2023-12-15 17:06:06 +0100

    pltotting

--- Rolling_Dice_App/sitka_highs.py
+++ Rolling_Dice_App/sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt 
+
 path = Path('weather data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
 
@@ -13,4 +15,11 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+#Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+
+
+# print(highs)
